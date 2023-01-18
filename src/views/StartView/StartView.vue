0cae0ba46cc44232110a341d91a1cf00e7e18f9b
White11010 <beliavski26@gmail.com>
2023-01-18 23:01:43 +0300

    add global button style

--- src/views/StartView/StartView.vue
+++ src/views/StartView/StartView.vue
@@ -2,92 +2,68 @@
   <section class="start">
     <ul class="start__actions">
       <li class="start__action">
-        <div
-          class="start__button"
-          @click="onPlayButtonClick"
-        >
-          <p class="start__button-label">PLAY</p>
-        </div>
+        <button class="start__button button-main" @click="onPlayButtonClick">
+          PLAY
+        </button>
       </li>
       <li class="start__action">
-        <div class="start__button">
-          <p class="start__button-label">OPTIONS</p>
-        </div>
+        <button class="start__button button-main">OPTIONS</button>
       </li>
       <li class="start__action">
-        <div class="start__button">
-          <p class="start__button-label">RULES</p>
-        </div>
+        <button class="start__button button-main">RULES</button>
       </li>
     </ul>
   </section>
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router'
+  import { useRouter } from "vue-router";
 
-const router = useRouter();
+  const router = useRouter();
 
+  const onPlayButtonClick = (): void => {
+    router.push({ name: "StartGameSettings" });
+  };
 
-const onPlayButtonClick = (): void => {
-  router.push({name: 'StartGameSettings'})
-}
-
-
-
-const onStartButtonClick = (name: string): void => {
-  router.push({
-    name: 'Lobby',
-    params: {
-      lobbyId: Date.now().toString(),
-    },
-    query: {
-      name: name,
-    }
-  })
-}
-
+  const onStartButtonClick = (name: string): void => {
+    router.push({
+      name: "Lobby",
+      params: {
+        lobbyId: Date.now().toString(),
+      },
+      query: {
+        name: name,
+      },
+    });
+  };
 </script>
 
 <style lang="scss" scoped>
-.start {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #A6CA59;
-
-  &__actions {
+  .start {
+    width: 100vw;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-    gap: 24px;
-    list-style: none;
-  }
+    background-color: $green-light;
 
-  &__button {
-    width: 300px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid brown;
-    background-color: #9E900F;
-    border-radius: 20px;
-    box-sizing: border-box;
-    padding: 24px 36px;
-    cursor: pointer;
+    &__actions {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      gap: 24px;
+      list-style: none;
+    }
 
-    &:hover {
-      background-color: #93820d;
+    &__button {
+      width: 300px;
+    }
+
+    &__button-label {
+      font-family: sans-serif;
+      font-weight: 600;
+      color: #cfed19;
     }
   }
-
-  &__button-label {
-    font-family: sans-serif;
-    font-weight: 600;
-    color: #CFED19;
-  }
-}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/global.scss";`,
+        additionalData: `@import "@/assets/global.scss"; @import "@/assets/variables.scss";`,
       },
     },
   },
