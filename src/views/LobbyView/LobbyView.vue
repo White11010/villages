e9dc5fed36621eb947d24cac6f4cefa2cde6add2
White11010 <beliavski26@gmail.com>
2023-01-18 22:42:07 +0300

    add global scss

--- src/views/LobbyView/LobbyView.vue
+++ src/views/LobbyView/LobbyView.vue
@@ -1,72 +1,81 @@
 <template>
   <section class="lobby">
     <div class="lobby__players">
-      <div
-        v-for="(player, index) in lobbyState.playersList"
-        :key="index"
-      >
+      <div v-for="(player, index) in lobbyState.playersList" :key="index">
         {{ player.name }}
       </div>
     </div>
-    <div
-      class="lobby__join-button"
-      @click="createLinkToJoin"
-    >
-      <p>Copy link to join</p>
+    <div class="lobby__actions">
+      <div class="lobby__join-button" @click="createLinkToJoin">
+        <p>Copy link to join</p>
+      </div>
+      <div class="button lobby__start-button">
+        <p>Copy link to join</p>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-import {onMounted, ref, reactive} from 'vue'
-import {io} from "socket.io-client";
-import {useRoute} from 'vue-router';
+  import { onMounted, ref, reactive, onBeforeUnmount } from "vue";
+  import { io } from "socket.io-client";
+  import { useRoute } from "vue-router";
 
-const socket = io('http://localhost:3000');
-const route = useRoute();
+  const socket = io("http://localhost:3000");
+  const route = useRoute();
 
-onMounted(() => {
-  socket.emit('joinRoom', {
-    room: route.params.lobbyId,
-    name: route.query.name,
+  onMounted(() => {
+    socket.emit("joinRoom", {
+      room: route.params.lobbyId,
+      name: route.query.name,
+    });
+
+    socket.on("playerJoined", data => {
+      console.log(data);
+
+      lobbyState.playersList = data.users;
+    });
+
+    socket.on("playerLeft", data => {
+      lobbyState.playersList = data.users;
+    });
   });
 
-  socket.on('playerJoined', (data) => {
-    console.log(data);
-    
-    lobbyState.playersList = data.users;
-  })
-})
+  onBeforeUnmount(() => {
+    socket.emit("leaveRoom", {
+      room: route.params.lobbyId,
+      name: route.query.name,
+    });
+  });
 
+  const lobbyState = reactive({
+    playersList: [],
+  });
 
-const lobbyState = reactive({
-  playersList: []
-});
-
-
-const createLinkToJoin = () => {
-  navigator.clipboard.writeText(`${window.location.origin}/start-game-settings?lobbyId=${route.params.lobbyId}`);
-}
+  const createLinkToJoin = () => {
+    navigator.clipboard.writeText(
+      `${window.location.origin}/start-game-settings?lobbyId=${route.params.lobbyId}`
+    );
+  };
 </script>
 
 <style lang="scss" scoped>
-.lobby {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 24px;
-
-  &__join-button {
-    padding: 8px 20px;
-    border-radius: 10px;
-    border: 1px solid black;
+  .lobby {
+    width: 100vw;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    cursor: pointer;
+    gap: 24px;
+
+    &__join-button {
+      padding: 8px 20px;
+      border-radius: 10px;
+      border: 1px solid black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
   }
-}
 </style>
--- src/views/StartGameSettingsView/StartGameSettingsView.vue
+++ src/views/StartGameSettingsView/StartGameSettingsView.vue
@@ -2,14 +2,8 @@
   <section class="game-settings">
     <div class="game-settings__content">
       <div class="game-settings__avatar"></div>
-      <input
-        v-model.trim="name"
-        class="game-settings__name-input"
-      >
-      <button
-        class="game-settings__button"
-        @click="onStartButtonClick"
-      >
+      <input v-model.trim="name" class="game-settings__name-input" />
+      <button class="game-settings__button" @click="onStartButtonClick">
         Start
       </button>
     </div>
@@ -17,85 +11,82 @@
 </template>
 
 <script lang="ts" setup>
-import {ref, computed} from 'vue';
-import { useRouter, useRoute } from 'vue-router'
+  import { ref, computed } from "vue";
+  import { useRouter, useRoute } from "vue-router";
 
-const router = useRouter();
-const route = useRoute();
+  const router = useRouter();
+  const route = useRoute();
 
-const name = ref<string>('');
+  const name = ref<string>("");
 
-const isJoining = computed(() => {
-  return !!route.query.lobbyId;
-})
+  const isJoining = computed(() => {
+    return !!route.query.lobbyId;
+  });
 
-
-const onStartButtonClick = (): void => {
-  console.log(isJoining.value)
-  router.push({
-    name: 'Lobby',
-    params: {
-      lobbyId: isJoining.value ? route.query.lobbyId : Date.now().toString(),
-    },
-    query: {
-      name: name.value,
-    }
-  })
-}
+  const onStartButtonClick = (): void => {
+    router.push({
+      name: "Lobby",
+      params: {
+        lobbyId: isJoining.value ? route.query.lobbyId : Date.now().toString(),
+      },
+      query: {
+        name: name.value,
+      },
+    });
+  };
 </script>
 
 <style lang="scss" scoped>
-.game-settings {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #A6CA59;
-
-
-  &__content {
-    width: 600px;
-    height: 400px;
-    background-color: #d0c66f;
-    border-radius: 15px;
-    border: 5px solid #483f0c;
+  .game-settings {
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    flex-direction: column;
-    align-items: center;
     justify-content: center;
-    gap: 40px;
-  }
+    align-items: center;
+    background-color: #a6ca59;
 
-  &__avatar {
-    width: 100px;
-    height: 120px;
-    border-radius: 40%;
-    background-color: #483f0c;
-  }
+    &__content {
+      width: 600px;
+      height: 400px;
+      background-color: #d0c66f;
+      border-radius: 15px;
+      border: 5px solid #483f0c;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 40px;
+    }
 
-  &__name-input {
-    width: 300px;
-    //height: 40px;
-    outline: none;
-    font-size: 30px;
-    box-sizing: border-box;
-    padding: 8px;
-  }
+    &__avatar {
+      width: 100px;
+      height: 120px;
+      border-radius: 40%;
+      background-color: #483f0c;
+    }
 
-  &__button {
-    background-color: #708c22;
-    color: #CFED19;
-    border: none;
-    padding: 8px 16px;
-    font-size: 40px;
-    font-family: sans-serif;
-    border-radius: 15px;
-    cursor: pointer;
+    &__name-input {
+      width: 300px;
+      //height: 40px;
+      outline: none;
+      font-size: 30px;
+      box-sizing: border-box;
+      padding: 8px;
+    }
 
-    &:hover {
-      background-color: #4c6215;
+    &__button {
+      background-color: #708c22;
+      color: #cfed19;
+      border: none;
+      padding: 8px 16px;
+      font-size: 40px;
+      font-family: sans-serif;
+      border-radius: 15px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #4c6215;
+      }
     }
   }
-}
 </style>
